ff5fa70386c00c7a820c4ae7e4b27c4a6fa092d1
Heidi Hlavinka <hchlav@gmail.com>
2022-11-01 10:13:55 -0500

    commit tree 9601218f0dbe28dddf50400fba1e1a24007be35b

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE books.series_id = 1 ORDER BY books.year;"
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY books.year;"    
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters ORDER BY(characters.motto) LIMIT 1;"
+  "SELECT name, motto FROM characters ORDER BY motto LIMIT 1;"
 end
 
 
@@ -12,14 +12,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors INNER JOIN series ON series.author_id = authors.id INNER JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters ON characters.author_id = series.author_id ORDER BY series.title LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*) FROM character_books INNER JOIN characters ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY COUNT(*) DESC, characters.name;"
 end
 
